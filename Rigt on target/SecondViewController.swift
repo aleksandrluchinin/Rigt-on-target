d34fbee3ebb6933afd7c9e70c426ead4607cb8a1
saturn777 <aleksandr-luchinin@bk.ru>
2022-12-01 17:30:58 -0800

    Изменение логики

---
+++ Rigt on target/SecondViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SecondViewController.swift
+//  Rigt on target
+//
+//  Created by Aleksandr Luchinin on 01.12.2022.
+//
+
+//import UIKit
+//
+//class SecondViewController: UIViewController {
+//    
+//    override func loadView() {
+//        super.loadView()
+//        
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//       
+//    }
+//    
+////    @IBAction func backButton(_ sender: Any) {
+////        self.dismiss(animated: true, completion: nil)
+////    }
+//    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        
+//    }
+//    
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        
+//    }
+//    
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        
+//    }
+//    
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//        
+//    }
+//}
--- Rigt on target/ViewController.swift
+++ Rigt on target/ViewController.swift
@@ -8,51 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var mySlider: UISlider!
-    @IBOutlet weak var numberLabel: UILabel!
     
-    var number: Int = 0
-    var round: Int = 0
-    var points: Int = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func loadView() {
+        super.loadView()
         
+        let versionLabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))
+        versionLabel.text = "Версия 1.1"
+        view.addSubview(versionLabel)
     }
-
-    @IBAction func numberButton(_ sender: Any) {
+    @IBOutlet var slider: UISlider!
+        @IBOutlet var label: UILabel!
         
-        if round == 0 {
-            number = Int.random(in: 1...50)
-            numberLabel.text = String(number)
-            round = 1
-        } else {
-            let numSlider = Int(mySlider.value.rounded())
-            
-            if numSlider > number {
-                points += 50 - numSlider + number
-            } else if numSlider < number {
-                points += 50 - number + numSlider
-            } else {
-                points += 50
-            }
-            if round == 50 {
-                
-                let alert = UIAlertController(
-                title: "Игра окончена",
-                message: "Вы заработали \(self.points) очков", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
-                self.present(alert, animated: true, completion: nil); self.round = 1
-                self.points = 0
-            } else {
-                round += 1
-            }
-            number = Int.random(in: 1...50)
-            numberLabel.text = String(number)
-            }
-            
+        // загаданное число
+        var number: Int = 0
+        // раунд
+        var round: Int = 0
+        // сумма очков за раунд
+        var points: Int = 0
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
         }
-    }
+        
+        @IBAction func checkNumber() {
+            // если игра только начинается
+            if self.round == 0 {
+                // генерируем случайное число
+                self.number = Int.random(in: 1...50)
+              // передаем значение случайного числа в label
+                self.label.text = String(self.number)
+                // устанавливаем счетчик раундов на 1
+                self.round = 1
+            } else {
+                // получаем значение на слайдере
+                let numSlider = Int(self.slider.value.rounded())
+                print(numSlider)
+                // сравниваем значение с загаданным
+                // и подсчитываем очки
+                if numSlider > self.number {
+                    self.points += 50 - numSlider + self.number
+                } else if numSlider < self.number {
+                    self.points += 50 - self.number + numSlider
+                } else {
+                    self.points += 50
+                }
+                print("Очки - \(self.points)")
+                if self.round == 5 {
+                    // выводим информационное окно
+                    // с результатами игры
+                    let alert = UIAlertController(
+                        title: "Игра окончена",
+                        message: "Вы заработали \(self.points) очков",
+                        preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    self.round = 1
+                    self.points = 0
+                } else {
+                    self.round += 1
+                }
+                // генерируем случайное число
+                self.number = Int.random(in: 1...50)
+          // передаем значение случайного числа в label
+                self.label.text = String(self.number)
+            }
+        }
 
+    }
 
